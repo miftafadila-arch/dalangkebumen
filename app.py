d651a18fd77c6eee2342c75cce553220fe687290
miftafadila-arch <mifta.fadila@mhs.unsoed.ac.id>
2026-07-15 13:47:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ gap:12px;
 
 <img src="https://raw.githubusercontent.com/miftafadila-arch/dalangkebumen/main/logo.png"
          width="30">
-<img src="https://raw.githubusercontent.com/miftafadila-arch/dalangkebumen/main/logometro.webp"
+<img src="https://raw.githubusercontent.com/miftafadila-arch/dalangkebumen/main/logometro-removebg-preview.png"
          width="30">
 
 </div>
